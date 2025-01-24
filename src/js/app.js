c25e1894c0af3a21206e52cac2ae5ab9778a2605
mohammad mahdi ganjalia <mohammad00mahdi00ganji@gmail.com>
2025-01-24 10:25:30 +0330

    complete section footer and complete project

--- src/js/app.js
+++ src/js/app.js
@@ -9,27 +9,32 @@ menuBar.addEventListener("click", function(){
     if(flagMenuBar){
         menuBarLineCenter.classList.remove("bg-white")
         menuBarLineCenter.classList.add("bg-reansparent")
+
         menuBarLineBottom.classList.remove("translate-y-2")
-        menuBarLineBottom.classList.add("rotate-45")
+        menuBarLineBottom.classList.add("bar-bottom")
+
         menuBarLineTop.classList.remove("-translate-y-2")
-        menuBarLineTop.classList.add("-rotate-45")
-        menuMobile.classList.remove("h-0")
-        menuMobile.classList.add("h-[430px]")
-        menuMobile.classList.remove("invisible")
-        menuMobile.classList.add("visible")
+        menuBarLineTop.classList.add("bar-top")
+
+        menuMobile.classList.remove("close__menu")
+        menuMobile.classList.add("open__menu")
+       
+
         flagMenuBar = false
 
     }else{
         menuBarLineCenter.classList.add("bg-white")
         menuBarLineCenter.classList.remove("bg-reansparent")
+
         menuBarLineBottom.classList.add("translate-y-2")
-        menuBarLineBottom.classList.remove("rotate-45")
+        menuBarLineBottom.classList.remove("bar-bottom")
+
         menuBarLineTop.classList.add("-translate-y-2")
-        menuBarLineTop.classList.remove("-rotate-45")
-        menuMobile.classList.add("h-0")
-        menuMobile.classList.remove("h-[430px]")
-        menuMobile.classList.add("invisible")
-        menuMobile.classList.remove("visible")
+        menuBarLineTop.classList.remove("bar-top")
+
+        menuMobile.classList.remove("open__menu")
+        menuMobile.classList.add("close__menu")
+
         flagMenuBar = true
 
     }
